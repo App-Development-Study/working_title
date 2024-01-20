8e2a7f0dff244ea6fd2e0a4c1183419a4f7d2a0d
HamBeomJoon <37996727+HamBeomJoon@users.noreply.github.com>
2024-01-20 23:16:04 +0900

    Feat: 소셜 로그인 (#5)
    * Feat: 파이어베이스 연동
    
    * Feat: 소셜 로그인 연동
    
    * Feat: Drawer 생성
    
    * Feat: Pretendard 폰트추가

--- sad/lib/main.dart
+++ sad/lib/main.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:sad/splash.dart';
 
-void main() {
-  runApp(MyApp());
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:sad/login_page/loginpage.dart';
+import 'package:sad/splash.dart';
+// import 'package:sad/login_page/google_loginpage.dart';
+
+void main() async {
+  // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initKakaoSdk(); // 비동기 함수 호출
+  // print(await KakaoSdk.origin);
+  runApp(const MyApp());
+}
+
+Future<void> initKakaoSdk() async {
+  KakaoSdk.init(nativeAppKey: 'ddb61127a6856b0d518fce6aa9ae9e92');
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Splash();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter LoginPage',
+      theme: ThemeData(
+        // primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: "Pretendard",
+      ),
+      home: const LoginPage(),
+    );
   }
 }
